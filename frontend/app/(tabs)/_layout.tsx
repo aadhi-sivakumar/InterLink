e9cf2dd265a3535c95f90f3123d6d4e4c1da069b
dnng1 <143498949+dnng1@users.noreply.github.com>
2025-10-22 02:00:21 -0500

    set up project using node, react native, expo go, expo navigation, updated gitignore

---
+++ frontend/app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#007AFF",
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
